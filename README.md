dd8ad39e8ba7306ccc1c026b1015dcfc1d9a555f
amirhosain sabzevari <ah.sabzevari3771@gmail.com>
2024-03-29 13:22:04 +0330

    inisial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Website project, designed with tailwind CSS
+
+***
+
+Link to view the website online:
+
+***
+
+![alt text](images/img-readme/127.0.0.1_5500_public_indext.html.png)
---
+++ public/script.js
@@ -0,0 +1,17 @@
+var glide07 = new Glide('.glide-08', {
+    type: 'slider',
+    focusAt: 'center',
+    perView: 1,
+    autoplay: 3500,
+    animationDuration: 700,
+    gap: 0,
+    classes: {
+        activeNav: '[&>*]:bg-slate-700',
+    },
+});
+
+glide07.mount();
+
+
+
+
